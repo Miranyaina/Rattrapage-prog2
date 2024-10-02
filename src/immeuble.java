33f0c2b9d4d03018a0020081bc73739270c902f9
Miranyaina <hei.mira.110@gmail.com>
2024-10-02 16:54:16 +0300

    Creation de la classe Immeuble

---
+++ src/immeuble.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class immeuble extends endroit {
+    private int nombreDEtages;
+    private List<appartement> appartement;
+
+    public immeuble(int id, String nom, int nombreDEtages) {
+        super(id, nom);
+        this.nombreDEtages = nombreDEtages;
+        this.appartement = new ArrayList<>();
+    }
+
+    public void ajouterAppartement(appartement appartement) {
+        this.appartement.add(appartement);
+    }
+
+    public int compterAppartements() {
+        return appartement.size();
+    }
+
+    public List<appartement> getAppartement() {
+        return appartement;
+    }
+}
