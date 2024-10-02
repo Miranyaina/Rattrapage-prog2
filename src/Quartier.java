e8c4b3d68e4145877ed22c00c5f9c943267cbd77
Miranyaina <hei.mira.110@gmail.com>
2024-10-02 17:15:11 +0300

    Determination de la presence d'un appartement dans le quartier

--- src/Quartier.java
+++ src/Quartier.java
@@ -11,4 +11,26 @@ public abstract class Quartier extends endroit{
     public void ajouterImmeuble(immeuble immeubles) {
         this.immeubles.add(immeubles);
     }
+
+    public boolean contientAppartement(appartement appartement) {
+        for (immeuble immeuble : immeubles) {
+            if (immeuble.getAppartement().contains(appartement)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int compterLesAppartements() {
+        int totalAppartements = 0;
+        for (immeuble immeuble : immeubles) {
+            totalAppartements += immeuble.compterLesAppartements();
+        }
+        return totalAppartements;
+    }
+
+    public List<immeuble> getImmeubles() {
+        return immeubles;
+    }
 }
